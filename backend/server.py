1dec305787b41a5464d776ea072cc9d71892104d
Georgio Feghali <75293296+GeorgioFe@users.noreply.github.com>
2024-05-31 21:35:48 -0700

    Connected User Authentication to UI.

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
+import auth
 import uvicorn
 import os
 
@@ -9,7 +10,7 @@ app = FastAPI()
 # Mount the static files directories
 app.mount("/static", StaticFiles(directory=os.path.join(os.path.dirname(__file__), '..', 'frontend', 'static')), name="static")
 
-# Route to Root
+# Route for Root.
 @app.get("/", response_class=HTMLResponse)
 def render_root():
     # Using a relative path to the HTML file
@@ -18,7 +19,7 @@ def render_root():
         html_content = file.read()
     return HTMLResponse(content=html_content, status_code=200)
 
-# Route to About Us
+# Route for About Us Page.
 @app.get("/about", response_class=HTMLResponse)
 def render_about():
     # Using a relative path to the HTML file
@@ -27,7 +28,7 @@ def render_about():
         html_content = file.read()
     return HTMLResponse(content=html_content, status_code=200)
 
-# Route to Help
+# Route for Help Page.
 @app.get("/help", response_class=HTMLResponse)
 def render_help():
     # Using a relative path to the HTML file
@@ -36,14 +37,76 @@ def render_help():
         html_content = file.read()
     return HTMLResponse(content=html_content, status_code=200)
 
-# Route to Help
+# Route for Dashboard Page.
+@app.get("/dashboard", response_class=HTMLResponse)
+def render_dashboard():
+    # Using a relative path to the HTML file
+    file_path = os.path.join(os.path.dirname(__file__), '..', 'frontend', 'pages', 'dashboard.html')
+    with open(file_path, "r") as file:
+        html_content = file.read()
+    return HTMLResponse(content=html_content, status_code=200)
+
+# Route for Register Page.
 @app.get("/register", response_class=HTMLResponse)
-def render_help():
+def render_register():
     # Using a relative path to the HTML file
     file_path = os.path.join(os.path.dirname(__file__), '..', 'frontend', 'pages', 'register.html')
     with open(file_path, "r") as file:
         html_content = file.read()
     return HTMLResponse(content=html_content, status_code=200)
+
+# Route for Login Page.
+@app.get("/login", response_class=HTMLResponse)
+def render_login():
+    # Using a relative path to the HTML file
+    file_path = os.path.join(os.path.dirname(__file__), '..', 'frontend', 'pages', 'signin.html')
+    with open(file_path, "r") as file:
+        html_content = file.read()
+    return HTMLResponse(content=html_content, status_code=200)
+
+# Route for Sign Up Button.
+@app.post("/signup", response_class=HTMLResponse)
+def signup(email: str = Form(...), password: str = Form(...)):
+    # Sign up user
+    response = auth.user_sign_up(email, password)
+    
+    # HTML Content to be Returned
+    content = """
+    <html>
+        <head>
+            <script type="text/javascript">
+                alert("Signup successful!");
+                window.location.href = "/";
+            </script>
+        </head>
+        <body>
+        </body>
+    </html>
+    """
+    
+    return HTMLResponse(content=content, status_code=200)
+
+# Route for Sign In Page.
+@app.post("/signin", response_class=HTMLResponse)
+def signin(email: str = Form(...), password: str = Form(...)):
+    # Sign in user
+    response = auth.user_sign_in(email, password)
+    
+    # HTML Content to be Returned
+    content = """
+    <html>
+        <head>
+            <script type="text/javascript">
+                alert("Sign-in successful!");
+                window.location.href = "/dashboard";
+            </script>
+        </head>
+        <body>
+        </body>
+    </html>
+    """
+    
+    return HTMLResponse(content=content, status_code=200)
 
 # Run the server
 if __name__ == "__main__":
--- backend/test.py
+++
@@ -1,9 +0,0 @@
-from supabase_setup import supabase
-import auth
-import db_access as db
-
-# Sign in user
-auth.user_sign_in('justtesting@gmail.com', 'yoyo123')
-
-# Sign out user
-auth.user_sign_out()
